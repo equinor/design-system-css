de9da4b9c5ac5862724e9abe87fca4d8fc59ea7e
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-11 15:27:10 +0200

    started autocomplete

---
+++ packages/components/src/autocomplete/Autocomplete.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Autocomplete } from './Autocomplete'
+
+const meta = {
+  title: 'Surfaces/Autocomplete',
+  component: Autocomplete,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+        <Story />
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof Autocomplete>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+        <Autocomplete {...args} />
+  )},
+}
---
+++ packages/components/src/autocomplete/Autocomplete.tsx
@@ -0,0 +1,30 @@
+import './autocomplete.css'
+import '../input/Input'
+import { Input } from '../input/Input'
+import { useRef } from 'react'
+
+export const Autocomplete = () => {
+  const options = useRef<HTMLDivElement>(null)
+  const openPopover = () => {
+    options.current?.showPopover()
+  }
+  const closePopover = () => {
+    options.current?.hidePopover()
+  }
+
+  return (
+  <>
+  {/* @ts-ignore */}
+  <Input onFocus={openPopover} style={{anchorName: '--input'}} />
+  {/* @ts-ignore */}
+  <div ref={options} className="eds-autocomplete__options" popover="manual" onClick={closePopover}>
+    <ul>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+  </div>
+  </>
+)}
