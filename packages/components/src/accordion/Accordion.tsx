6a33cada0b9c62fdba1daadedb0f1937bdc7cca8
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-12 16:25:30 +0200

    icon component

--- packages/components/src/accordion/Accordion.tsx
+++ packages/components/src/accordion/Accordion.tsx
@@ -1,10 +1,13 @@
 import './accordion.css'
 import { UIText } from '../'
+import {Icon, chevronRight} from '../'
 
 type AccordionProps = React.DetailsHTMLAttributes<HTMLDetailsElement>
 export const Accordion = ({children, ...rest}: AccordionProps) => (
   <details className={`eds-accordion`} {...rest}>
     <summary>
+
+      <span className="eds-accordion__icon"><Icon path={chevronRight} /></span>
       <span className="eds-accordion__heading"><UIText>Hello</UIText></span>
       <span className="eds-accordion__actions"><UIText>Save</UIText><UIText>Delete</UIText></span>
     </summary>
---
+++ packages/components/src/icon/Icon.tsx
@@ -0,0 +1,10 @@
+export const Icon = ({ path, size = 24 }: { path: string; size?: number }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox={`0 0 ${size} ${size}`}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path fillRule="evenodd" clipRule="evenodd" d={path} />
+  </svg>
+)
