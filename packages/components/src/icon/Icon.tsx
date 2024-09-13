6ccb863622ecdf95fdd9d55081c16749b12e8ecd
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-13 10:19:32 +0200

    icon: scale to size

--- packages/components/src/icon/Icon.tsx
+++ packages/components/src/icon/Icon.tsx
@@ -3,14 +3,17 @@ type Props = {
   path: string
   size?: number
 } & React.SVGAttributes<SVGElement>
-export const Icon = ({ path, size = 24, ...rest }: Props) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox={`0 0 ${size} ${size}`}
-    xmlns="http://www.w3.org/2000/svg"
-    {...rest}
-  >
-    <path fillRule="evenodd" clipRule="evenodd" d={path} />
-  </svg>
-)
+export const Icon = ({ path, style, size = 24, ...rest }: Props) => {
+  const pathSize = size / 24
+  return (
+    <svg
+      style={{fontSize: `${size}px`, ...style}}
+      width={size}
+      height={size}
+      viewBox={`0 0 ${size} ${size}`}
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      <path style={{scale: `${pathSize}`}} fillRule="evenodd" clipRule="evenodd" d={path} />
+    </svg>
+  )}
