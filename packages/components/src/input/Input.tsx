32cc3e36afd7fad6f2a7e5f622d5e398302e5568
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-18 10:43:37 +0200

    input: left/right adornments

--- packages/components/src/input/Input.tsx
+++ packages/components/src/input/Input.tsx
@@ -3,11 +3,15 @@ import './input.css'
 type Props = {
   variant?: "error" | "warning" | "success"
   disabled?: boolean
+  leftAdornments?: React.ReactNode
+  rightAdornments?: React.ReactNode
 
 } & React.HTMLAttributes<HTMLInputElement>
 
-export const Input = ({ variant, disabled, style, className, ...rest }: Props) => (
+export const Input = ({ variant, disabled, style, className, leftAdornments, rightAdornments, ...rest }: Props) => (
   <div style={style} className={`eds-input ${variant ? variant : 'initial'} ${className ? className : ''}`} >
+    <span className="adornments left">{leftAdornments}</span>
     <input disabled={disabled} type="text" {...rest} />
+    <span className="adornments right">{rightAdornments}</span>
   </div>
 )
