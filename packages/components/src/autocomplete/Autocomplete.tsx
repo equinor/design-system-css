16de9131c13c30e06dba1d3530a8bf1c7bfefabc
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-18 11:38:05 +0200

    autocomplete: more styles

--- packages/components/src/autocomplete/Autocomplete.tsx
+++ packages/components/src/autocomplete/Autocomplete.tsx
@@ -1,6 +1,6 @@
 import './autocomplete.css'
 import '../input/Input'
-import { Input } from '../input/Input'
+import { Input, UIText, Icon, arrow_drop_down } from '../'
 import { useRef } from 'react'
 
 export const Autocomplete = () => {
@@ -14,16 +14,22 @@ export const Autocomplete = () => {
 
   return (
   <>
-    {/* @ts-ignore */}
-    <Input onFocus={openPopover} style={{anchorName: '--input'}} />
+
+    <Input
+      onFocus={openPopover}
+      /* @ts-ignore */
+      style={{anchorName: '--input'}}
+      rightAdornments={<Icon size={24}
+      path={arrow_drop_down}
+      className="eds-autocomplete__input-icon" />} />
     {/* @ts-ignore */}
     <div ref={options} className="eds-autocomplete__options" popover="manual" onClick={closePopover}>
       <ul>
-        <li><button>item</button></li>
-        <li><button>item</button></li>
-        <li><button>item</button></li>
-        <li><button>item</button></li>
-        <li><button>item</button></li>
+        <li><UIText>Kahoot! ASA</UIText></li>
+        <li><UIText>Microsoft Corporation</UIText></li>
+        <li><UIText>Testla Inc</UIText></li>
+        <li><UIText>Apple Inc</UIText></li>
+        <li><UIText>NVIDIA corporation</UIText></li>
       </ul>
     </div>
   </>
