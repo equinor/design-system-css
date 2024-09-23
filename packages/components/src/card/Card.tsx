9a026aba0cabfbd4f3609f907c699ab65084d6f9
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-23 12:27:11 +0200

    card wip

--- packages/components/src/card/Card.tsx
+++ packages/components/src/card/Card.tsx
@@ -5,7 +5,7 @@ type Props = {
   variant?: 'default' | 'info' | 'warning' | 'error'
 } & React.HTMLAttributes<HTMLDivElement>
 export const Card = ({children, className, elevation = 'none', variant = 'default', ...rest }: Props) => {
-  const elevationParsed = elevation === 'none' ? '' : elevation
+  const elevationParsed = `elevation-${elevation}`
   return (
    <div className={['eds-card', elevationParsed, variant, className].join(' ')} {...rest}>{children}</div>
   )}
