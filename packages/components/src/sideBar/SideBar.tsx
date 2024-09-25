302dd53eefae660e7e5e2153a3e27baf3d722edb
oddvernes <oddbjorn.overnes@gmail.com>
2024-09-25 12:51:35 +0200

    sidebar is always spacious i think

--- packages/components/src/sideBar/SideBar.tsx
+++ packages/components/src/sideBar/SideBar.tsx
@@ -5,7 +5,7 @@ import { account_circle, gear, help_outline, log_out, expand } from '../icon'
 import './sidebar.css'
 type Props = HTMLAttributes<HTMLDivElement>
 export const SideBar = ({className, ...rest }: Props) => (
-  <div className={['eds-sidebar', className].join(' ')} {...rest}>
+  <div data-density="spacious" className={['eds-sidebar', className].join(' ')} {...rest}>
     <div className="eds-sidebar__main">
       <a href="#" className="eds-sidebar__link"><Icon path={account_circle} /></a>
       <a href="#" className="eds-sidebar__link"><Icon path={gear} /></a>
