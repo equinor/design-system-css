328d12a813ef18577e5cd7f92b5eb559b1c6dbc5
oddvernes <oddbjorn.overnes@gmail.com>
2024-10-31 15:37:36 +0100

    ds colors for card

--- packages/components/src/card/Card.stories.tsx
+++ packages/components/src/card/Card.stories.tsx
@@ -24,6 +24,7 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: (args) => {
     return (
+      <>
         <Card {...args}>
           <div className="eds-card__content u-rich-text">
             <BodyText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi eaque non, tenetur eos ut, vitae ipsum voluptate cum excepturi asperiores corrupti quibusdam, doloremque <strong>Multiline</strong> with inline icons <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> adipisci in eveniet nihil? Aliquid, tenetur.</BodyText>
@@ -31,6 +32,14 @@ export const Default: Story = {
             <Button><Icon icon={add_circle_outlined} />A Button</Button>
           </div>
           </Card>
+            <Card {...args} className="colors-from-ds">
+            <div className="eds-card__content u-rich-text">
+              <BodyText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi eaque non, tenetur eos ut, vitae ipsum voluptate cum excepturi asperiores corrupti quibusdam, doloremque <strong>Multiline</strong> with inline icons <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> adipisci in eveniet nihil? Aliquid, tenetur.</BodyText>
+              <BodyText size='xl'>This is one line with <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> inline icons</BodyText>
+              <Button><Icon icon={add_circle_outlined} />A Button</Button>
+            </div>
+            </Card>
+      </>
   )},
 }
 
