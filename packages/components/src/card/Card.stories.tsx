6bd05cd3b094f26ad6d8589813e4c7a33b860a3e
oddvernes <oddbjorn.overnes@gmail.com>
2025-01-27 12:17:41 +0100

    card: add ds colors

--- packages/components/src/card/Card.stories.tsx
+++ packages/components/src/card/Card.stories.tsx
@@ -24,22 +24,13 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: (args) => {
     return (
-      <>
-        <Card {...args}>
-          <div className="eds-card__content u-rich-text">
-            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi eaque non, tenetur eos ut, vitae ipsum voluptate cum excepturi asperiores corrupti quibusdam, doloremque <strong>Multiline</strong> with inline icons <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> adipisci in eveniet nihil? Aliquid, tenetur.</Text>
-            <Text size='xl'>This is one line with <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> inline icons</Text>
-            <Button><Icon icon={add_circle_outlined} />A Button</Button>
-          </div>
-          </Card>
-            <Card {...args} className="colors-from-ds">
-            <div className="eds-card__content u-rich-text">
-              <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi eaque non, tenetur eos ut, vitae ipsum voluptate cum excepturi asperiores corrupti quibusdam, doloremque <strong>Multiline</strong> with inline icons <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> adipisci in eveniet nihil? Aliquid, tenetur.</Text>
-              <Text size='xl'>This is one line with <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> inline icons</Text>
-              <Button><Icon icon={add_circle_outlined} />A Button</Button>
-            </div>
-            </Card>
-      </>
+      <Card {...args}>
+        <div className="eds-card__content u-rich-text">
+          <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi eaque non, tenetur eos ut, vitae ipsum voluptate cum excepturi asperiores corrupti quibusdam, doloremque <strong>Multiline</strong> with inline icons <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> adipisci in eveniet nihil? Aliquid, tenetur.</Text>
+          <Text size='xl'>This is one line with <Icon size='xs' icon={add_circle_outlined} /><Icon size='sm' icon={add_circle_outlined} /><Icon size='md' icon={add_circle_outlined} /><Icon size='lg' icon={add_circle_outlined} /><Icon size='xl' icon={add_circle_outlined} /> inline icons</Text>
+          <Button><Icon icon={add_circle_outlined} />A Button</Button>
+        </div>
+      </Card>
   )},
 }
 
--- packages/components/src/card/Card.tsx
+++ packages/components/src/card/Card.tsx
@@ -1,13 +1,11 @@
 import './card.css'
 
 type Props = {
-  elevation?: 'none' | 'raised' | 'overlay'
+  //elevation?: 'none' | 'raised' | 'overlay'
   variant?: 'default' | 'info' | 'warning' | 'error'
-  token?: boolean
 } & React.HTMLAttributes<HTMLDivElement>
-export const Card = ({children, className, token, elevation = 'none', variant = 'default', ...rest }: Props) => {
-  const elevationParsed = `elevation-${elevation}`
-  const useToken = token ? 'colors-from-tokens' : ''
+export const Card = ({children, className, variant = 'default', ...rest }: Props) => {
+  //const elevationParsed = `elevation-${elevation}`
   return (
-   <div className={['eds-card', elevationParsed, useToken, variant, className].join(' ')} {...rest}>{children}</div>
+   <div className={['eds-card', /* elevationParsed */, variant, className].join(' ')} {...rest}>{children}</div>
   )}
