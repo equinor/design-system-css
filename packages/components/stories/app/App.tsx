28ce86e3868d362cfdb767219060ea0f6be04108
oddvernes <oddbjorn.overnes@gmail.com>
2025-01-28 10:58:22 +0100

    add cards to example app

--- packages/components/stories/app/App.tsx
+++ packages/components/stories/app/App.tsx
@@ -1,4 +1,4 @@
-import {Text, SideBar, Autocomplete} from '../../src'
+import {Text, SideBar, Autocomplete, Card} from '../../src'
 
 import './app.css';
 
@@ -10,7 +10,36 @@ export const App = () => {
       <section className="app__sidebar"><SideBar /></section>
       <section className="app__content">
         <Text variant='header' size="6xl" as="h1">Title</Text>
-        <Autocomplete />
+        <div className="card-grid">
+          <Card>
+            <div className="eds-card__content u-rich-text">
+            <Autocomplete />
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            </div>
+          </Card>
+          <Card variant="success">
+            <div className="eds-card__content u-rich-text">
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            </div>
+          </Card>
+          <Card  variant="info">
+            <div className="eds-card__content u-rich-text">
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            </div>
+          </Card>
+          <Card variant='warning'>
+            <div className="eds-card__content u-rich-text">
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            </div>
+          </Card>
+          <Card variant='error'>
+            <div className="eds-card__content u-rich-text">
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+            </div>
+          </Card>
+        </div>
+
       </section>
     </main>
   );
