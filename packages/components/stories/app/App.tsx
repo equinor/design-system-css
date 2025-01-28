bb309063b117c6eae20abfe320d92ceb239e5bb2
oddvernes <oddbjorn.overnes@gmail.com>
2025-01-28 12:59:10 +0100

    app: adding some form elements

--- packages/components/stories/app/App.tsx
+++ packages/components/stories/app/App.tsx
@@ -1,4 +1,4 @@
-import {Text, SideBar, Autocomplete, Card} from '../../src'
+import {Accordion, Text, SideBar, Autocomplete, Card, Radio, Checkbox} from '../../src'
 
 import './app.css';
 
@@ -10,11 +10,29 @@ export const App = () => {
       <section className="app__sidebar"><SideBar /></section>
       <section className="app__content">
         <Text variant='header' size="6xl" as="h1">Title</Text>
-        <div className="card-grid">
+        <div className="u-card-grid">
           <Card>
             <div className="eds-card__content u-rich-text">
-            <Autocomplete />
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+              <Autocomplete />
+              <form>
+                <div className="u-flex">
+                  <fieldset className='u-fieldset'>
+                    <legend>Radios</legend>
+                    <label className='eds-label'><Radio name="a" /><Text>Radio button</Text></label>
+                    <label className='eds-label'><Radio name="a" /><Text>Radio button</Text></label>
+                    <label className='eds-label'><Radio name="a" /><Text>Radio button</Text></label>
+                  </fieldset>
+                  <fieldset title="checkboxes" className='u-fieldset'>
+                    <legend>Checkboxes</legend>
+                    <label className='eds-label'><Checkbox /><Text>Checkbox</Text></label>
+                    <label className='eds-label'><Checkbox /><Text>Checkbox</Text></label>
+                    <label className='eds-label'><Checkbox /><Text>Checkbox</Text></label>
+                  </fieldset>
+                </div>
+              </form>
+              <Accordion>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nihil ea ut! Magni officiis est inventore, molestias autem minus numquam ea alias tempora rerum nisi quo deserunt beatae odio expedita!</Text>
+              </Accordion>
             </div>
           </Card>
           <Card variant="success">
