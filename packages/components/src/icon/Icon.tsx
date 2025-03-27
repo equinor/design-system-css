e2cb3a686442b63aa62ae1beb2678f3e93eb70a7
oddvernes <oddbjorn.overnes@gmail.com>
2025-03-27 20:38:44 +0100

    Icon: support custom viewbox size

--- packages/components/src/icon/Icon.tsx
+++ packages/components/src/icon/Icon.tsx
@@ -1,16 +1,22 @@
 
 import './icon.css'
+import { CSSProperties } from 'react'
 import { TypographySize } from '../'
 import type { IconData } from '@equinor/eds-icons'
 export type IconProps = {
   icon: IconData
   size?: TypographySize
 } & React.SVGAttributes<SVGElement>
-export const Icon = ({ icon, className, size = 'lg', ...rest }: IconProps) => {
+export const Icon = ({ icon, className, style, size = 'lg', ...rest }: IconProps) => {
   const sizeClass = `size-${size}`
+  const mergedStyle = {
+    '--viewbox-size': icon.height,
+    ...style
+  } as CSSProperties
   return (
     <svg
       className={['eds-icon', sizeClass, className].join(' ')}
+      style={mergedStyle}
       {...rest}
     >
       {Array.isArray(icon.svgPathData) ? (
